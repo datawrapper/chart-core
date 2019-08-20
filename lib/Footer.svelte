39230d5ecd270c574a5f4d8bdc5104df49bf1335
Ivan Lokhov <ilokhov@users.noreply.github.com>
2019-08-20 18:17:15 +0200

    render basemap attribution (#1)
    * render basemap attribution
    
    * remove hiding separator with css
    
    * put source into source-block
    
    * footer: show separator in all blocks except for the first one

--- lib/Footer.svelte
+++ lib/Footer.svelte
@@ -19,6 +19,17 @@
         e.target.focus();
         e.target.select();
     }
+
+    // show separator in all blocks except for the first one
+    let isFirstBlock = true;
+
+    function separator() {
+        if (isFirstBlock) {
+            isFirstBlock = false;
+            return '';
+        }
+        return '<span class="separator"></span>';
+    }
 </script>
 
 {#if chartBasedOn.byline}
@@ -38,21 +49,31 @@
 {/if}
 
 {#if source.name && data.sourcePosition !== 'above-footer'}
-    <span class="separator" />
-    {data.sourceCaption}:
-    {#if source.url}
-        <a class="source" target="_blank" rel="noopener noreferrer" href={source.url}>
-            {source.name}
-        </a>
-    {:else}{source.name}{/if}
+    <span class="footer-block source-block">
+        {@html separator()}
+        {data.sourceCaption}:
+        {#if source.url}
+            <a class="source" target="_blank" rel="noopener noreferrer" href={source.url}>
+                {source.name}
+            </a>
+        {:else}{source.name}{/if}
+    </span>
 {/if}
 
 <!-- hook chart_footer_after_source -->
 
+{#if data.basemapAttribution}
+    <span class="footer-block map-attribution-block">
+        {@html separator()}
+        {data.basemapAttribution.text}:
+        {@html data.basemapAttribution.caption}
+    </span>
+{/if}
+
 {#if data.getTheData.enabled}
     <span class="footer-block chart-action-data">
         {#if data.getTheData.caption}
-            <span class="separator" />
+            {@html separator()}
         {/if}
         <a href="data">{data.getTheData.caption}</a>
     </span>
@@ -61,7 +82,7 @@
 {#if data.embed.enabled}
     <span class="footer-block embed">
         {#if data.embed.caption}
-            <span class="separator" />
+            {@html separator()}
         {/if}
         <a href="#embed" class="chart-action-embed" on:click={handleClick}>{data.embed.caption}</a>
         <div class="embed-code {modalIsHidden ? 'hide' : ''}">
@@ -77,7 +98,7 @@
 {#if data.staticImage.enabled}
     <span class="footer-block static-image">
         {#if data.staticImage.caption}
-            <span class="separator" />
+            {@html separator()}
         {/if}
         <a href={`//img.datawrapper.de/${chartId}/full.png`} class="chart-action-image">
             {data.staticImage.caption}
@@ -87,7 +108,7 @@
 
 {#if data.createdWithDatawrapper}
     <span class="footer-block attribution">
-        <span class="separator" />
+        {@html separator()}
         <a href="#placeholder" target="_blank" rel="noopener noreferrer">
             {data.createdWithCaption} Datawrapper
         </a>
