93eddb0f1197f5d13f4d75e73b3d15c7101e089f
Fabian Gündel <f.guendel@gmail.com>
2019-09-11 15:33:32 +0200

    fix: bring publish inline with sapper rendering

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ import 'core-js/stable';
 import Chart from './lib/Chart.svelte';
 
 const chart = new Chart({
-    target: document.body,
+    target: document.querySelector('.dw-chart'),
     props: {
         data: window.__DW_DATA__,
         theme: window.__DW_THEME__,
