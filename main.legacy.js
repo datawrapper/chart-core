56ba7f39dcb85ca5a9c621f35faa2cab2b52f825
Fabian Gündel <f.guendel@gmail.com>
2020-01-13 09:16:19 +0100

    docs: change comment and add more information

--- main.legacy.js
+++ main.legacy.js
@@ -1,3 +1,6 @@
-/* there should be no need to edit this file. Look into `main.js` */
+/*
+ * There should be no need to edit this file. Look into `main.js`.
+ * The only purpose of this file is to inject polyfills for the legacy bundle which loads in IE.
+ */
 import 'core-js/stable';
 import './main.js';
