e8d931ceb54743c28d0721a22701568c0dafe26e
Fabian Gündel <5798652+sto3psl@users.noreply.github.com>
2020-03-09 14:49:57 +0100

    feat: simplify directory discovery for package consumers

---
+++ index.js
@@ -0,0 +1,15 @@
+const path = require('path');
+const fs = require('fs');
+
+module.exports = {
+    path: {
+        core: __dirname,
+        dist: path.join(__dirname, '/dist'),
+        lib: path.join(__dirname, '/lib'),
+        vendor: path.join(__dirname, '/dist/core'),
+        locale: path.join(__dirname, '/dist/core/locale')
+    },
+    svelte: require(path.join(__dirname, '/dist/core', 'Chart_SSR.js')),
+    script: require(path.join(__dirname, '/dist/core', 'manifest.json')),
+    less: path.join(__dirname, '/lib/styles.less')
+};
