dd6d9798a8f206c3feabbbeb1ced350efb84396c
Fabian Gündel <5798652+sto3psl@users.noreply.github.com>
2020-03-09 14:17:30 +0100

    feat(BREAKING): simplify data loading

--- main.js
+++ main.js
@@ -1,14 +1,8 @@
 import Chart from './lib/Chart.svelte';
 
-const data = window.__DW_DATA__;
-
 const chart = new Chart({
     target: document.body,
-    props: {
-        data,
-        theme: window.__DW_THEME__,
-        translations: window.__DW_TRANSLATIONS__
-    },
+    props: window.__DW_SVELTE_PROPS__,
     hydrate: true
 });
 
