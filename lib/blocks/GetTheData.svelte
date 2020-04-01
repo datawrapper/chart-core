45678d9a8d3a3ac552a1de116c291170090d1e38
Gregor Aisch <gka@users.noreply.github.com>
2020-04-01 14:55:42 +0200

    include chart title in aria-label caption for getTheData

--- lib/blocks/GetTheData.svelte
+++ lib/blocks/GetTheData.svelte
@@ -1,13 +1,19 @@
 <script>
     // external props
     export let props;
-    const { get, __ } = props;
+    const { get, __, purifyHtml } = props;
     $: theme = props.theme;
+    $: chart = props.chart;
 
     // internal props
     $: getTheData = get(theme, 'data.options.footer.getTheData', { enabled: false });
 </script>
 
 {#if getTheData.enabled}
-    <a class="dw-data-link" href="data">{__(getTheData.caption)}</a>
+    <a
+        class="dw-data-link"
+        aria-label="{__(getTheData.caption)}: {purifyHtml(chart.title, '')}"
+        href="data">
+        {__(getTheData.caption)}
+    </a>
 {/if}
