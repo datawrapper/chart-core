77e77d078a5613215a5fcf490bca2d4b65b24285
Fabian Gündel <5798652+sto3psl@users.noreply.github.com>
2020-04-09 16:38:48 +0200

    fix: select chart root from id

--- main.js
+++ main.js
@@ -5,7 +5,7 @@ import Chart from './lib/Chart.svelte';
 
 function render() {
     new Chart({
-        target: document.body,
+        target: document.getElementById('__svelte-dw'),
         props: window.__DW_SVELTE_PROPS__,
         hydrate: true
     });
