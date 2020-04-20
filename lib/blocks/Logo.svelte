709909cf163cffc264327c2f50138e3c5156650d
Gregor Aisch <gka@users.noreply.github.com>
2020-04-20 11:36:08 +0200

    allow html in logo.text

--- lib/blocks/Logo.svelte
+++ lib/blocks/Logo.svelte
@@ -1,18 +1,18 @@
 <script>
     // external props
     export let props;
-    const { get } = props;
+    const { get, purifyHtml } = props;
     $: theme = props.theme;
 
     // internal props
     $: logo = get(theme, 'data.options.footer.logo', { enabled: false });
 </script>
 
-{#if logo.enabled && logo.position === 'right'}
-    {#if logo.url}
-        <img height={logo.height} src={logo.url} alt={theme.title} />
-    {/if}
-    {#if logo.text}
-        <span class="logo-text">{logo.text}</span>
-    {/if}
+{#if logo.url}
+    <img height={logo.height} src={logo.url} alt={theme.title} />
+{/if}
+{#if logo.text}
+    <span class="logo-text">
+        {@html purifyHtml(logo.text)}
+    </span>
 {/if}
