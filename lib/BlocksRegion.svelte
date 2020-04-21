237e5cafba5c8bf3b8b1111462e99d209daa0b95
Gregor Aisch <gka@users.noreply.github.com>
2020-04-21 23:55:47 +0200

    only render a BlocksRegion outer div if there are blocks

--- lib/BlocksRegion.svelte
+++ lib/BlocksRegion.svelte
@@ -5,22 +5,24 @@
     export let blocks;
 </script>
 
-<div class={name}>
-    {#each blocks as block}
-        <div class="block block-{block.id}" class:export-text={block.exportText}>
-            {#if block.prepend}
-                <span class="prepend">
-                    {@html clean(block.prepend)}
+{#if blocks.length}
+    <div class={name}>
+        {#each blocks as block}
+            <div class="block block-{block.id}" class:export-text={block.exportText}>
+                {#if block.prepend}
+                    <span class="prepend">
+                        {@html clean(block.prepend)}
+                    </span>
+                {/if}
+                <span class="block-inner">
+                    <svelte:component this={block.component} props={block.props} />
                 </span>
-            {/if}
-            <span class="block-inner">
-                <svelte:component this={block.component} props={block.props} />
-            </span>
-            {#if block.append}
-                <span class="append">
-                    {@html clean(block.append)}
-                </span>
-            {/if}
-        </div>
-    {/each}
-</div>
+                {#if block.append}
+                    <span class="append">
+                        {@html clean(block.append)}
+                    </span>
+                {/if}
+            </div>
+        {/each}
+    </div>
+{/if}
