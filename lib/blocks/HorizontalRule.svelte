82ce68b3e119d214857bfd982671e2f7be0c5a64
Gregor Aisch <gka@users.noreply.github.com>
2020-07-03 20:25:14 +0000

    allows for up to 3 blocks for horizontal lines (#37)
    * allows for up to 3 blocks for horizontal lines
    
    * use class dw-line to include hr in print exports
    
    * margin property for hr-block
    
    Co-authored-by: Elana <19191012+elanals@users.noreply.github.com>

---
+++ lib/blocks/HorizontalRule.svelte
@@ -0,0 +1,12 @@
+<script>
+    // external props
+    export let props;
+    const { get } = props;
+    $: theme = props.theme;
+
+    $: data = get(theme, `data.options.blocks.${props.id}.data`, {});
+    $: border = get(data, 'border', '1px solid #cccccc');
+    $: margin = get(data, 'margin', '0px');
+</script>
+
+<hr class="dw-line" style="border: 0; border-bottom: {border}; margin: {margin};" />
