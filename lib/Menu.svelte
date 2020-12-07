496ac31842c525b9efe4bcc72e1cf342500dff7c
David Kokkelink <david@datawrapper.de>
2020-12-07 13:09:08 +0100

    Display menu when configured in theme (#68)
    * add menu
    
    * change class
    
    * update css
    
    * update hover
    
    * css, thicker hamburger lines, box shadow, links font color initial
    
    * don't render menu in static mode
    
    Co-authored-by: Elana <19191012+elanals@users.noreply.github.com>

---
+++ lib/Menu.svelte
@@ -0,0 +1,85 @@
+<script>
+    import BlocksRegion from './BlocksRegion.svelte';
+
+    export let id;
+    export let name;
+    export let blocks;
+
+    let open = false;
+
+    function toggle() {
+        open = !open;
+    }
+
+    function hide() {
+        open = false;
+    }
+</script>
+
+<style>
+    .menu {
+        cursor: pointer;
+
+        position: absolute;
+        top: 0px;
+        right: 0px;
+        margin: 4px 10px 3px 3px;
+        width: 18px;
+        padding: 3px 0px;
+
+        border-top: 2px solid black;
+        border-bottom: 2px solid black;
+    }
+
+    .menu div {
+        height: 2px;
+        width: 100%;
+        background: black;
+    }
+
+    .menu:hover {
+        border-top-color: #ccc;
+        border-bottom-color: #ccc;
+    }
+
+    .menu:hover div {
+        background: #ccc;
+    }
+
+    .menu-content {
+        position: absolute;
+        top: 30px;
+        right: 4px;
+        background: white;
+        border: 1px solid #ccc;
+        z-index: 1;
+        box-shadow: 0px 0px 4px 1px rgba(0, 0, 0, 0.1);
+    }
+
+    :global(.menu-content > .dw-chart-menu a) {
+        padding: 10px;
+        display: block;
+        color: initial;
+        border-bottom: 1px solid #ccc;
+    }
+
+    :global(.menu-content > .dw-chart-menu a:hover) {
+        background: rgba(0, 0, 0, 0.05);
+    }
+
+    :global(.menu-content > .dw-chart-menu .block:last-child a) {
+        border-bottom: none;
+    }
+</style>
+
+<svelte:window on:click={hide} />
+
+{#if blocks.length}
+    <div class="menu tooltip" on:click|stopPropagation={toggle}>
+        <div />
+    </div>
+
+    <div class="menu-content tooltip" on:click|stopPropagation class:hidden={!open}>
+        <BlocksRegion {id} {name} {blocks} />
+    </div>
+{/if}
