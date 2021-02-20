0027930211828d7372f727570254953510aeb1ad
David Kokkelink <david@datawrapper.de>
2021-02-20 19:03:54 +0100

    update index

--- index.js
+++ index.js
@@ -15,6 +15,6 @@ module.exports = {
     },
     svelte: require(path.join(__dirname, '/dist', 'Visualization_SSR.js')),
     less: path.join(__dirname, '/lib/styles.less'),
-    css: path.join(__dirname, '/dist', 'web-component-content.css'),
+    css: path.join(__dirname, '/dist', 'web-component.css'),
     polyfills: fs.readdirSync(polyfillDirectory).map(file => path.join(polyfillDirectory, file))
 };
