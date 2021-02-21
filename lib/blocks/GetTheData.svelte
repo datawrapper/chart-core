a8cbf5fc08fc6cecd40c2872f76d9fe85454e2d4
David Kokkelink <david@datawrapper.de>
2021-02-21 14:51:18 +0100

    move get-the-data logic to svelte component

--- lib/blocks/GetTheData.svelte
+++ lib/blocks/GetTheData.svelte
@@ -1,18 +1,51 @@
 <script>
+    import { onMount } from 'svelte';
     // external props
     export let props;
     const { get, __, purifyHtml } = props;
-    $: ({ chart, theme } = props);
+    $: ({ chart, data, theme } = props);
 
     // internal props
     $: getTheData = get(theme, 'data.options.footer.getTheData', { enabled: false });
+
+    let dataLink;
+    let hidden = false;
+    let href = 'data';
+    let download = '';
+
+    onMount(async function() {
+        // update data link to point to edited dataset
+        const dataset = await dw.chart(chart).load(data || '', chart.externalData);
+
+        const csv = dataset.toCSV && dataset.toCSV();
+
+        if (!csv || (csv && csv.trim && csv.trim() === 'X.1')) {
+            hidden = true;
+        } else {
+            if (window.navigator.msSaveOrOpenBlob) {
+                const blobObject = new Blob([csv]);
+                dataLink.addEventListener('click', event => {
+                    window.navigator.msSaveOrOpenBlob(blobObject, 'data-' + chart.id + '.csv');
+                    event.preventDefault();
+                    return false;
+                });
+            } else {
+                download = 'data-' + chart.id + '.csv';
+                href =
+                    'data:application/octet-stream;charset=utf-8,' +
+                    encodeURIComponent('\uFEFF' + csv);
+            }
+        }
+    });
 </script>
 
-{#if getTheData.enabled}
+{#if getTheData.enabled && !hidden}
     <a
+        this="dataLink"
         class="dw-data-link"
         aria-label="{__(getTheData.caption)}: {purifyHtml(chart.title, '')}"
-        href="data">
+        {download}
+        {href}>
         {__(getTheData.caption)}
     </a>
 {/if}
