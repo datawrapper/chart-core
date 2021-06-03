0dd528d7d9dd428c306f79dbb5e74b3238947dbd
elanals <19191012+elanals@users.noreply.github.com>
2021-06-03 17:22:44 +0200

    Don't pipe theme captions through translator (#138)

--- lib/blocks/Source.svelte
+++ lib/blocks/Source.svelte
@@ -5,13 +5,13 @@
     $: ({ chart, theme } = props);
 
     // internal props
-    $: caption = get(theme, 'data.options.blocks.source.data.caption');
+    $: caption = get(theme, 'data.options.blocks.source.data.caption', __('Source'));
     $: sourceName = purifyHtml(get(chart, 'metadata.describe.source-name'));
     $: sourceUrl = get(chart, 'metadata.describe.source-url');
 </script>
 
 {#if sourceName}
-    <span class="source-caption">{__(caption)}:</span>
+    <span class="source-caption">{caption}:</span>
     {#if sourceUrl}
         <a class="source" target="_blank" rel="noopener noreferrer" href={sourceUrl}>
             {@html sourceName}
